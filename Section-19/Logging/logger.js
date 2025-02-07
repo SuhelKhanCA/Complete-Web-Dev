ffcdc9b8fa1da6110ce8eb51359de8b672389fdd
suhelkhanca <suhel.hata@gmail.com>
2025-02-08 00:05:10 +0530

    Making my own logger system : Event Driven architecture

--- Section-19/Logging/logger.js
+++ Section-19/Logging/logger.js
@@ -2,12 +2,12 @@ const fs = require("fs");
 
 const os = require("os");
 
-const EventEmitter = require("events");
+const EventEmitter = require('events')
 
-class Logger extends EventEmitter {
-  log(message) {
-    this.emit("Message", { message });
-  }
+class Logger extends EventEmitter{
+    log(message) {
+        this.emit("message", {message})
+    }
 }
 
 const logger = new Logger();
@@ -15,15 +15,15 @@ const logger = new Logger();
 const logFile = "./event.log";
 
 const logToFile = (event) => {
-  const logMessage = `${new Date().toISOString()} - ${event.message} \n`;
-  fs.appendSync(logFile, logMessage);
-};
+    const logMessage = `${new Date().toISOString()} - ${event.message} \n`;
+    fs.appendFileSync(logFile, logMessage);
+}
 
-logger.on("message", logToFile);
+logger.on('message', logToFile);
 
 setInterval(() => {
-  const memoryUsage = Math.floor((os.freemem() / os.totalmem()) * 100);
-  logger.log(`Current memory usage : ${memoryUsage}`);
+    const memoryUsage = Math.floor((os.freemem() / os.totalmem()) * 100);
+    logger.log(`Current memory usage : ${memoryUsage}%`);
 }, 3000);
 
 logger.log("Application Started");
